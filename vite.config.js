5ae991b83fa2944370a2d583fe7d5d675a1d01f2
h1ut <amirosmonalievgg@gmail.com>
2025-10-05 11:01:31 +0600

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: import.meta.env.VITE_BASE_PATH || "/parade-weather-app",
 })
