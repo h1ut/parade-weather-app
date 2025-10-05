dc64e055b9a620ebca566b0efa0ce7542d2a7040
h1ut <amirosmonalievgg@gmail.com>
2025-10-05 11:24:54 +0600

    fix: vite config and add vercel routing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   //base: process.env.VITE_BASE_PATH || "/parade-weather-app",
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || "/parade-weather-app",
+  base: "/parade-weather-app",
 })
